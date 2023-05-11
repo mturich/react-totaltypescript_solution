df1170a6bb641d4328c191f80bab53ee2dbfc3c0
Michael Urich <urich.michael@outlook.com>
2023-05-11 22:14:19 +0200

    ex 26

--- src/04-advanced-components/22-discriminated-union-props.problem.tsx
+++ src/04-advanced-components/22-discriminated-union-props.problem.tsx
@@ -1,32 +1,36 @@
 // How to use discriminated unions in props
 // With variant
 
-type ModalProps = {
-  variant: "no-title" | "title";
-  title?: string;
-};
+type ModalProps =
+   | {
+        variant: 'title'
+        title: string
+     }
+   | {
+        variant: 'no-title'
+     }
 
 export const Modal = (props: ModalProps) => {
-  if (props.variant === "no-title") {
-    return <div>No title</div>;
-  } else {
-    return <div>Title: {props.title}</div>;
-  }
-};
+   if (props.variant === 'no-title') {
+      return <div>No title</div>
+   } else {
+      return <div>Title: {props.title}</div>
+   }
+}
 
 export const Test = () => {
-  return (
-    <div>
-      <Modal variant="title" title="Hello" />
-      <Modal variant="no-title" />
+   return (
+      <div>
+         <Modal variant='title' title='Hello' />
+         <Modal variant='no-title' />
 
-      {/* @ts-expect-error */}
-      <Modal />
-      <Modal
-        variant="no-title"
-        // @ts-expect-error
-        title="Oh dear"
-      />
-    </div>
-  );
-};
+         {/* @ts-expect-error */}
+         <Modal />
+         <Modal
+            variant='no-title'
+            // @ts-expect-error
+            title='Oh dear'
+         />
+      </div>
+   )
+}
